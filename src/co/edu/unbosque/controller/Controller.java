5c090ab1828d567ffbb62585ef872e840ddf037d
Santiago Porras <sporrass@unbosque.edu.co>
2021-12-03 10:38:51 -0500

    Añadiendo menu y Javadoc

--- src/co/edu/unbosque/controller/Controller.java
+++ src/co/edu/unbosque/controller/Controller.java
@@ -1,12 +1,99 @@
 package co.edu.unbosque.controller;
 
+import co.edu.unbosque.view.View;
+/**
+ * Clase de Controller, donde unimos la vista con los métodos que hay en el
+ * modelo, en este caso los algoritmos a realizar.
+ * 
+ * @authors Santiago Porras Sepúlveda, Fabian Camilo Gomez, Omar Felipe Ladino y Juan Felipe Rojas
+ *
+ */
+
 public class Controller {
+	/**
+	 * vista de tipo View.
+	 */
+	
+	private View vista;
+	
+	/**
+	 * Constructor de la clase Controller en donde se inicializan las clases que
+	 * tenemos como atributos.
+	 */
 
 	public Controller() {
 
+		vista= new View();
+		run();
 	}
+	/**
+	 * Constructor de la clase Controller en donde se inicializan las clases que
+	 * tenemos como atributos.
+	 */
 
 	private void run() {
+		
+		int decision = 0;
+		String dec = "";
+		do {
+			vista.show("Bienvenido al programa proyecto final de Complejidad: ");
+			dec = vista.write("[1] Problema Nutricionista \n[2] Problema de la liga de Futbol \n[3] Problema del conejo\n"
+				 + "[4] Salir");
+			try {
+				if (vista.exceptionNumber(dec)) {
+				}
+			} catch (Exception e1) {
+				vista.show("Señor Usuario, escriba bien los números\n");
+
+			}
+
+			switch (dec) {
+			case "1":
+				String t = vista.write(" Ingreso 1 ");
+				try {
+					if (vista.exceptionNumber(t)) {
+						
+
+					}
+				} catch (Exception e1) {
+					vista.show("Ingrese bien los numeros");
+				}
+
+				break;
+
+			case "2":
+
+				String n1 = vista.write("Ingreso 2 ");
+				try {
+				
+					
+					
+
+				} catch (Exception e) {
+					vista.show("Escriba bien el numero");
+				}
+
+				break;
+
+			case "3":
+
+				String t3 = vista.write("Ingreso 3");
+				try {
+					
+				} catch (Exception e) {
+					vista.show("Escriba bien el numero");
+				}
+
+				break;
+
+			case "4":
+				break;
+			default:
+				vista.show("Escoga alguna opcion valida");
+			}
+
+		} while (!dec.equals("4"));
+
 
 	}
 }
--- src/co/edu/unbosque/view/View.java
+++ src/co/edu/unbosque/view/View.java
@@ -1,5 +1,88 @@
 package co.edu.unbosque.view;
 
+import java.util.Scanner;
+
+/**
+ * Clase de vista, donde podemos hacer el metodo de escritura y mostrar en consola los problemas.
+ * 
+ * @authors Santiago Porras Sepúlveda, Fabian Camilo Gomez, Omar Felipe Ladino y Juan Felipe Rojas
+ *
+ */
+
 public class View {
+	/**
+	 * sc de tipo Scanner, donde inicializamos la clase para la escritura.
+	 */
+	
+	Scanner sc = new Scanner(System.in);
+	
+	/**
+	 * Constructor vacío para llamar los métodos de la clase.
+	 */
+	public View() {
+
+	}
+	
+	/**
+	 * Método para escribir lo que el usuario necesite.
+	 * 
+	 * @param mensaje de tipo String para mostrar el mensaje antes de la escritura.
+	 *                mensaje != null.
+	 * @return la variable escrito que es lo escrito por el usuario.
+	 */
+	public String write(String mess) {
+		System.out.println(mess);
+		String writen = sc.nextLine();
+		return writen;
+	}
+	
+	/**
+	 * Método de tipo void para imprimir el mensaje generado.
+	 * 
+	 * @param mensaje de tipo String para mostrar el mensaje antes de la escritura.
+	 *                mensaje != null.
+	 */
+	public void show(String mess) {
+		System.out.println(mess);
+	}
+	/**
+	 * Método para verificar si los datos ingresados por el usuario son solomante
+	 * números que escrito el usuario.
+	 * 
+	 * @param pNumber es de tipo String y son los datos del usuario. pNumber !=
+	 *                null.
+	 * @return un valor booleano para verificar si son números ingresados o no.
+	 */
+	
+	public boolean onlyNumber(String pNumber) {
+
+		boolean show = true;
+
+		if (!pNumber.trim().matches("[0-9]")) {
+			show = false;
+		}
+		return show;
+	}
+	
+	/**
+	 * Método que verifica y genera una excepción sobre, si el texto tiene números o
+	 * no.
+	 * 
+	 * @param pNumber es de tipo String y son los datos del usuario. pNumber !=
+	 *                null.
+	 * @return un valor booleano para verificar si son números ingresados o no.
+	 * @throws Exception
+	 */
+	
+	public boolean exceptionNumber(String pNumber) throws Exception {
+		boolean show;
+
+		if (onlyNumber(pNumber)) {
+			show = true;
+		} else {
+			throw new Exception("Error, solo escriba números");
+		}
+		return show;
+	}
 
 }
